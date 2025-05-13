8bd88ebb877de7113751d3485e97f086f76d9d21
Vittorio Postiglione <cupo-meridio@live.it>
2025-05-13 12:54:15 +0200

    Rimossa impostazione del progetto a moduli. Mo va tutto bene.

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module it.unisa.diem.wordageddong17 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.wordageddong17 to javafx.fxml;
-    exports it.unisa.diem.wordageddong17;
-}

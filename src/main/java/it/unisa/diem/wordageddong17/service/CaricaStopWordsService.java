cd03263839dff0f2b3bab086c3a760d9727861b2
Mattia <mattiasanzari2003@gmail.com>
2025-06-10 10:47:15 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/it/unisa/diem/wordageddong17/controller/SessionViewController.java
    Carica e prendi stopwords service

---
+++ src/main/java/it/unisa/diem/wordageddong17/service/CaricaStopWordsService.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.wordageddong17.service;
+
+import it.unisa.diem.wordageddong17.database.Database;
+import it.unisa.diem.wordageddong17.database.DatabaseStopWords;
+import it.unisa.diem.wordageddong17.interfaccia.DAOListaStopWords;
+import it.unisa.diem.wordageddong17.model.ListaStopWords;
+import it.unisa.diem.wordageddong17.model.Utente;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+/**
+ *
+ * @author Mattia Sanzari
+ */
+public class CaricaStopWordsService extends Service<Void> {
+    private final DAOListaStopWords dbSW;
+    private String nomeFile;
+    private String email;
+    private byte[] documento;
+
+    public CaricaStopWordsService(DAOListaStopWords dbSW, String nomeFile, String email, byte[] documento) {
+        this.dbSW = dbSW;
+        this.nomeFile = nomeFile;
+        this.email = email;
+        this.documento = documento;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDocumento(byte[] documento) {
+        this.documento = documento;
+    }
+    
+    
+
+    public void setNomeFile(String nomeFile) {
+        this.nomeFile = nomeFile;
+    }
+    
+    @Override
+    protected Task<Void> createTask() {
+     return new Task<Void>(){
+         @Override
+         protected Void call() throws Exception {
+            dbSW.CaricareStopwords(email, documento, nomeFile);
+            return null;
+         }
+     
+     };
+    }   
+}
